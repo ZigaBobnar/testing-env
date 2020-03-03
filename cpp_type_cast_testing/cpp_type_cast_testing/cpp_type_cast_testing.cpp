3a7d3a58d500380fb727f7cd094ded01461ec143
Ziga Bobnar <ziga@bobnar.net>
2020-03-03 20:50:04 +0100

    Testing dynamic cast

--- cpp_type_cast_testing/cpp_type_cast_testing/cpp_type_cast_testing.cpp
+++ cpp_type_cast_testing/cpp_type_cast_testing/cpp_type_cast_testing.cpp
@@ -10,15 +10,15 @@ public:
     string master_public = "public_default";
 
     Master() {
-        std::cout << "Master()" << std::endl;
+        //std::cout << "Master()" << std::endl;
     }
 
     Master(string master_public) : master_public(master_public) {
-
+        //std::cout << "Master()" << std::endl;
     }
 
     ~Master() {
-        std::cout << "~Master()" << std::endl;
+        //std::cout << "~Master()" << std::endl;
     }
 
     virtual std::string to_string() {
@@ -28,12 +28,35 @@ public:
 
 class Derived : public Master {
 public:
+    string derived_private = "private_derived_default";
+
     Derived() : Master("cds") {
-        std::cout << "Derived()" << std::endl;
+        //std::cout << "Derived()" << std::endl;
     }
 
     ~Derived() {
-        std::cout << "~Derived()" << std::endl;
+        //std::cout << "~Derived()" << std::endl;
+    }
+
+    virtual std::string to_string() {
+        return "Master string: " + Master::to_string() + "\nDerived:\nmaster_public: " + master_public + "\nderived_private: " + derived_private;
+    }
+};
+
+class Derived2 : public Master {
+public:
+    string derived_private = "private_derived2_default";
+
+    Derived2() : Master("ccdsacdsa") {
+        //std::cout << "Derived()" << std::endl;
+    }
+
+    ~Derived2() {
+        //std::cout << "~Derived()" << std::endl;
+    }
+
+    virtual std::string to_string() {
+        return "Master string: " + Master::to_string() + "\nDerived:\nmaster_public: " + master_public + "\nderived_private: " + derived_private;
     }
 };
 
@@ -41,7 +64,41 @@ public:
 
 int main()
 {
-    Derived d;
+    /*
 
-    std::cout << d.to_string() << std::endl;
+    Derived* d = new Derived();
+    d->derived_private = "custom";
+            
+    Master* m = d;
+    Master m1 = *m;
+
+    Derived* dm = dynamic_cast<Derived*>(m);
+    Derived2* dm2 = dynamic_cast<Derived2*>(m);
+
+    std::cout << d->to_string() << std::endl;
+    std::cout << std::endl;
+    std::cout << m->to_string() << std::endl;
+    std::cout << std::endl;
+    std::cout << m1.to_string() << std::endl;
+    std::cout << std::endl;
+    std::cout << dm->to_string() << std::endl;
+    std::cout << std::endl;
+    std::cout << dm2->to_string() << std::endl;
+    */
+
+    {
+        Derived* d = new Derived();
+        d->derived_private = "custom";
+
+        Master* m = dynamic_cast<Master*>(d);
+
+        Derived* dm = dynamic_cast<Derived*>(m);
+
+        std::cout << d->to_string() << std::endl;
+        std::cout << std::endl;
+        std::cout << m->to_string() << std::endl;
+        std::cout << std::endl;
+        std::cout << dm->to_string() << std::endl;
+        std::cout << std::endl;
+    }
 }
